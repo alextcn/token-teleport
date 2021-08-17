580b5a3f4c133d6ad325e9dd7793b2cac9e04944
Alex Tkachenko <skyalexx@gmail.com>
2021-08-17 18:14:03 +0300

    Register teleport pair test

--- test/EthTeleportAgent.test.js
+++ test/EthTeleportAgent.test.js
@@ -1,21 +1,28 @@
 const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 const { ZERO_ADDRESS } = constants;
 
+const ERC20Mock = artifacts.require('ERC20Mock');
 const EthTeleportAgent = artifacts.require('EthTeleportAgent');
 
-// TODO: 
-// - set fixed fee values
-// - deploy wrapped token
+
 contract('EthTeleportAgent', function (accounts) {
     const [sender, other] = accounts;
+
+    const name = 'My Token';
+    const symbol = 'MTKN';
 
     const registerFee = new BN(500);
     const teleportFee = new BN(100);
 
     beforeEach(async function () {
-        // TODO: deploy wrapped token implementation
+        // deploy token 
+        this.token = await ERC20Mock.new();
+        const x = await this.token.initialize(name, symbol, 18, sender);
+
+        // todo: deploy wrapped token implementation
         const wrappedTokenImpl = ZERO_ADDRESS;
 
+        // deploy teleport
         this.teleport = await EthTeleportAgent.new();
         await this.teleport.initialize(registerFee, teleportFee, sender, wrappedTokenImpl);
     });
@@ -61,10 +68,56 @@ contract('EthTeleportAgent', function (accounts) {
     });
 
     describe('register teleport pair', function () {
-        // todo: ...
+        it('can register by owner', async function () {
+            const tokenAddress = this.token.address;
+            const chainId = 56;
+            await this.teleport.registerTeleportPair(tokenAddress, chainId, { value: registerFee })
+            expect(await this.teleport.routesFromTokenToChain_(tokenAddress, chainId)).to.be.true;
+        });
+        it('anyone can register', async function () {
+            const tokenAddress = this.token.address;
+            const chainId = 56;
+            await this.teleport.registerTeleportPair(tokenAddress, chainId, { from: other, value: registerFee })
+            expect(await this.teleport.routesFromTokenToChain_(tokenAddress, chainId)).to.be.true;
+        });
+        it('can\'t register twice', async function () {
+            const tokenAddress = this.token.address;
+            const chainId = 56;
+            await this.teleport.registerTeleportPair(tokenAddress, chainId, { value: registerFee })
+            expect(await this.teleport.routesFromTokenToChain_(tokenAddress, chainId)).to.be.true;
+            
+            await expectRevert(
+                this.teleport.registerTeleportPair(tokenAddress, chainId, { value: registerFee }),
+                'already registered'
+            );
+        });
+        it('can\'t register non-contract', async function () {
+            await expectRevert(
+                this.teleport.registerTeleportPair(other, 56, { value: registerFee }),
+                'given address is not a contract'
+            );
+        });
+        it('fee is required', async function () {
+            const tokenAddress = this.token.address;
+            await expectRevert(
+                this.teleport.registerTeleportPair(tokenAddress, 56),
+                'fee mismatch'
+            );
+            await expectRevert(
+                this.teleport.registerTeleportPair(tokenAddress, 56, { value: registerFee.sub(new BN(1)) }),
+                'fee mismatch'
+            );
+        });
+        xit('can\'t register for this chain', async function () {
+            const chainId = 1; // todo: get current network id
+            await expectRevert(
+                this.teleport.registerTeleportPair(this.token.address, chainId, { value: registerFee }),
+                'no need to register teleport to original chain'
+            );
+        })
     });
 
-    describe('create teleport pair', function () {
+    xdescribe('create teleport pair', function () {
         it('pair not created', async function () {
             // todo: ...
         });
@@ -74,7 +127,7 @@ contract('EthTeleportAgent', function (accounts) {
         });
     });
 
-    describe('teleport start', function () {
+    xdescribe('teleport start', function () {
         describe('fee not paid', function () {
             it('reverts', async function () {
                 // call start teleport without msg.value less than teleportFee
@@ -92,7 +145,7 @@ contract('EthTeleportAgent', function (accounts) {
         });
     })
 
-    describe('teleport finish', function () {
+    xdescribe('teleport finish', function () {
         // todo: ...
     });
 
