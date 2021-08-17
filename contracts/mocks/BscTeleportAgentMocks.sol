521020a7eab00bb3cdf6fc27c93e5b0c377c9c3d
v <vladimirfol@gmail.com>
2021-08-17 21:48:26 +0300

    SafeERC20.test.js

--- contracts/mocks/BscTeleportAgentMocks.sol
+++ contracts/mocks/BscTeleportAgentMocks.sol
@@ -107,3 +107,141 @@ contract AddressImpl {
     // sendValue's tests require the contract to hold Ether
     receive() external payable {}
 }
+
+
+contract BEP20ReturnFalseMock is Context {
+    uint256 private _allowance;
+
+    // IERC20's functions are not pure, but these mock implementations are: to prevent Solidity from issuing warnings,
+    // we write to a dummy state variable.
+    uint256 private _dummy;
+
+    function transfer(address, uint256) public returns (bool) {
+        _dummy = 0;
+        return false;
+    }
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) public returns (bool) {
+        _dummy = 0;
+        return false;
+    }
+
+    function approve(address, uint256) public returns (bool) {
+        _dummy = 0;
+        return false;
+    }
+
+    function allowance(address, address) public view returns (uint256) {
+        require(_dummy == 0); // Duummy read from a state variable so that the function is view
+        return 0;
+    }
+}
+
+contract BEP20ReturnTrueMock is Context {
+    mapping(address => uint256) private _allowances;
+
+    // IERC20's functions are not pure, but these mock implementations are: to prevent Solidity from issuing warnings,
+    // we write to a dummy state variable.
+    uint256 private _dummy;
+
+    function transfer(address, uint256) public returns (bool) {
+        _dummy = 0;
+        return true;
+    }
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) public returns (bool) {
+        _dummy = 0;
+        return true;
+    }
+
+    function approve(address, uint256) public returns (bool) {
+        _dummy = 0;
+        return true;
+    }
+
+    function setAllowance(uint256 allowance_) public {
+        _allowances[_msgSender()] = allowance_;
+    }
+
+    function allowance(address owner, address) public view returns (uint256) {
+        return _allowances[owner];
+    }
+}
+
+contract BEP20NoReturnMock is Context {
+    mapping(address => uint256) private _allowances;
+
+    // IERC20's functions are not pure, but these mock implementations are: to prevent Solidity from issuing warnings,
+    // we write to a dummy state variable.
+    uint256 private _dummy;
+
+    function transfer(address, uint256) public {
+        _dummy = 0;
+    }
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) public {
+        _dummy = 0;
+    }
+
+    function approve(address, uint256) public {
+        _dummy = 0;
+    }
+
+    function setAllowance(uint256 allowance_) public {
+        _allowances[_msgSender()] = allowance_;
+    }
+
+    function allowance(address owner, address) public view returns (uint256) {
+        return _allowances[owner];
+    }
+}
+
+contract SafeBEP20Wrapper is Context {
+    using SafeBEP20 for IBEP20;
+
+    IBEP20 private _token;
+
+    constructor(IBEP20 token) {
+        _token = token;
+    }
+
+    function transfer() public {
+        _token.safeTransfer(address(0), 0);
+    }
+
+    function transferFrom() public {
+        _token.safeTransferFrom(address(0), address(0), 0);
+    }
+
+//    function approve(uint256 amount) public {
+//        _token.approve(address(0), amount);
+//    }
+
+//    function increaseAllowance(uint256 amount) public {
+//        _token.increaseAllowance(address(0), amount);
+//    }
+
+//    function decreaseAllowance(uint256 amount) public {
+//        _token.decreaseAllowance(address(0), amount);
+//    }
+
+    function setAllowance(uint256 allowance_) public {
+        BEP20ReturnTrueMock(address(_token)).setAllowance(allowance_);
+    }
+
+    function allowance() public view returns (uint256) {
+        return _token.allowance(address(0), address(0));
+    }
+}
